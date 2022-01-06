ed4e42d59458f310c233419a59b5d8493da4f25a
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2022-01-06 12:40:42 +0100

    Add choosing user type

--- src/redux/userRedux.js
+++ src/redux/userRedux.js
@@ -1,11 +1,29 @@
 /* selectors */
 export const getUser = ({ user }) => user;
+export const getUsers = ({ users }) => users;
 
 /* action name creator */
 const reducerName = "user";
 const createActionName = (name) => `app/${reducerName}/${name}`;
 
 /* action types */
-const CHANGE_STATUS = createActionName("CHANGE_STATUS");
+const CHANGE_USER = createActionName("CHANGE_USER");
 
-export const fetchStarted = (payload) => ({ payload, type: CHANGE_STATUS });
+/* action creators */
+export const changeUser = (payload) => ({ payload, type: CHANGE_USER });
+
+/* reducer */
+
+export const reducer = (statePart = [], action = {}) => {
+  switch (action.type) {
+    case CHANGE_USER: {
+      return {
+        name: action.payload.name,
+        logged: action.payload.logged,
+        position: action.payload.position,
+      };
+    }
+    default:
+      return statePart;
+  }
+};
